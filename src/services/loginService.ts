e0a8a3b4f5921ca06cd654959a9bb749c7a7aeca
yournightmare01 <zlatkor1001@gmail.com>
2022-05-31 16:05:41 +0200

    Made changes to login functionality

--- src/services/loginService.ts
+++ src/services/loginService.ts
@@ -4,7 +4,7 @@ import { User } from '../common/types';
 import { LOGIN_URL } from '../common/constants';
 
 export const LoginService = {
-  login: async (userData: User) => {
+  login: async (userData: User): Promise<User> => {
     const response = await httpService.post(LOGIN_URL, userData);
     return response.data;
   }
