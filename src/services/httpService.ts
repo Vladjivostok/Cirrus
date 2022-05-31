d11c7d1ec6e56b19904087e1b7b3877acc2d7f83
yournightmare01 <zlatkor1001@gmail.com>
2022-05-31 15:15:02 +0200

    Added .env file and changed baseURL in httpService.ts

--- src/services/httpService.ts
+++ src/services/httpService.ts
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: process.env.REACT_APP_BASE_URL,
   headers: { 'Content-Type': 'application/json' }
 });
-
-//htpp service sa crud operacijama
