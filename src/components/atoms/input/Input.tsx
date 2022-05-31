bcf37d854b755a493f8232364ff1b49929028942
Vladimir_Mokric <vladimir.mokric@htecgroup.com>
2022-05-31 12:51:17 +0200

    [CJQ2TB-19] Added error handling and validation

--- src/components/atoms/input/Input.tsx
+++ src/components/atoms/input/Input.tsx
@@ -3,14 +3,17 @@ import React from 'react';
 type InputType = 'text' | 'password';
 
 type InputProps = {
-  id: string;
   name: string;
   type: InputType;
   placeholder: string;
+  onChange: any;
+  onBlur: any;
 };
 
-const Input: React.FC<InputProps> = ({ id, name, type, placeholder }) => {
-  return <input id={id} name={name} type={type} placeholder={placeholder} />;
+const Input: React.FC<InputProps> = ({ name, type, placeholder, onChange, onBlur }) => {
+  return (
+    <input name={name} type={type} placeholder={placeholder} onChange={onChange} onBlur={onBlur} />
+  );
 };
 
 export default Input;
--- src/components/molecules/loginForm/LoginForm.tsx
+++ src/components/molecules/loginForm/LoginForm.tsx
@@ -1,13 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Input from '../../atoms/input/Input';
 import Button from '../../atoms/button/Button';
 
 const Form: React.FC = () => {
+  const [inputValues, setInputValues] = useState({ username: '', password: '' });
+
+  const [usernameError, setUsernameError] = useState({
+    isShown: false,
+    message: 'Username input cant be empty!'
+  });
+
+  const [passwordError, setPasswordError] = useState({
+    isShown: false,
+    message: 'Password input cant be empty!'
+  });
+
+  const onChangeHandler = (e: any) => {
+    setInputValues((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value
+    }));
+  };
+
+  const submitHandler = (event: any) => {
+    event.preventDefault();
+    console.log(inputValues);
+  };
+
+  const validateInput = (event: any) => {
+    switch (event.target.name) {
+      case 'username':
+        setUsernameError((prevError) => ({
+          ...prevError,
+          isShown: false
+        }));
+        if (event.target.value == '') {
+          setUsernameError((prevError) => ({
+            ...prevError,
+            isShown: true
+          }));
+        }
+        break;
+      case 'password':
+        setPasswordError((prevError) => ({
+          ...prevError,
+          isShown: false
+        }));
+        if (event.target.value == '') {
+          setPasswordError((prevError) => ({
+            ...prevError,
+            isShown: true
+          }));
+        }
+        break;
+
+      default:
+        break;
+    }
+  };
+
   return (
-    <form>
-      <Input id={''} name={'username'} type={'text'} placeholder={'Username'} />
-      <Input id={''} name={'password'} type={'password'} placeholder={'Password'} />
+    <form onSubmit={submitHandler}>
+      <Input
+        name={'username'}
+        type={'text'}
+        placeholder={'Username'}
+        onChange={onChangeHandler}
+        onBlur={validateInput}
+      />
+      {usernameError.isShown && <p>{usernameError.message}</p>}
+      <Input
+        name={'password'}
+        type={'password'}
+        placeholder={'Password'}
+        onChange={onChangeHandler}
+        onBlur={validateInput}
+      />
+      {passwordError.isShown && <p>{passwordError.message}</p>}
       <Button />
     </form>
   );
