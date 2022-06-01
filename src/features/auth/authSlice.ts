9187e255b7b85e53438fe5cd0610182513cbe1d2
yournightmare01 <zlatkor1001@gmail.com>
2022-06-01 15:22:40 +0200

    Added setup for redux

--- src/features/auth/authSlice.ts
+++ src/features/auth/authSlice.ts
@@ -1,20 +1,6 @@
 import { createSlice, PayloadAction, createAsyncThunk } from '@reduxjs/toolkit';
 
-import loginService from '../../services/loginService';
-
 import type { RootState } from '../../store/store';
-
-// export const loginThunk = createAsyncThunk('auth/login', async (userData, thunkAPI) => {
-//   try {
-//     return await loginService.login(userData);
-//   } catch (error: any) {
-//     const message =
-//       (error.response && error.response.data && error.response.data.message) ||
-//       error.message ||
-//       error.toString();
-//     return thunkAPI.rejectWithValue(message);
-//   }
-// });
 
 interface AuthState {
   isLoggedIn: boolean;
