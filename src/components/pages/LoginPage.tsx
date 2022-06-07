27dce56d535f4432f8d4106afef18c6efbf0bc2b
Vladimir Mokric <vladimir.mokric@htecgroup.com>
2022-06-07 11:47:34 +0000

    [CJQ2TB-18] Created Login form UI
    Approved-by: Gavrilo Aleksic
    Approved-by: Milica Djordjevic
    Approved-by: Zlatko Radenkovic

---
+++ src/components/pages/LoginPage.tsx
@@ -0,0 +1,96 @@
+import React, { useState } from 'react';
+
+import * as Yup from 'yup';
+import { useFormik, FormikErrors, FormikTouched } from 'formik';
+
+import logo from './../../../src/assets/Cirrus.png';
+import Input from '../atoms/input/Input';
+import Button from '../atoms/button/Button';
+import FormErrorMessage from '../atoms/errorMessage/FormErrorMessage';
+import { Hide, Show } from '../atoms/passwordIcon/Svg';
+import './loginPage.css';
+
+const LoginScheme = Yup.object().shape({
+  username: Yup.string().trim().required('Username required!'),
+  password: Yup.string().trim().required('Password required!')
+});
+
+const LoginPage: React.FC = () => {
+  const [toggleShowPassword, setToggleShowPassword] = useState(false);
+  const togglePassword = () => {
+    setToggleShowPassword(!toggleShowPassword);
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: ''
+    },
+    validationSchema: LoginScheme,
+    onSubmit: (values) => {
+      formik.setSubmitting(true);
+
+      formik.setSubmitting(false);
+      formik.resetForm();
+      setToggleShowPassword(false);
+      toggleIcon();
+    }
+  });
+
+  interface FormValues {
+    username: string | undefined;
+    password: string | undefined;
+  }
+
+  const errorMessage = (
+    errors: FormikErrors<FormValues>,
+    touched: FormikTouched<FormValues>,
+    inputType: 'username' | 'password'
+  ) => {
+    if (touched.username && inputType === 'username') {
+      return <FormErrorMessage>{errors.username}</FormErrorMessage>;
+    } else if (touched.password && inputType === 'password') {
+      return <FormErrorMessage>{errors.password}</FormErrorMessage>;
+    }
+  };
+  const toggleIcon = () => {
+    if (!toggleShowPassword) {
+      return <Show onClick={togglePassword}></Show>;
+    } else if (toggleShowPassword) {
+      return <Hide onClick={togglePassword}></Hide>;
+    }
+  };
+
+  return (
+    <div className="loginPage">
+      <form className="loginForm" onSubmit={formik.handleSubmit}>
+        <img src={logo} alt="Cirrus logo missing" />
+        <div className="inputWrapper">
+          <Input
+            name="username"
+            placeholder="Username"
+            type="text"
+            value={formik.values.username}
+            onChange={formik.handleChange}
+          />
+          {errorMessage(formik.errors, formik.touched, 'username')}
+        </div>
+        <div className="inputWrapper">
+          <Input
+            name="password"
+            placeholder="Password"
+            type={toggleShowPassword ? 'text' : 'password'}
+            value={formik.values.password}
+            onChange={formik.handleChange}
+          />
+          {errorMessage(formik.errors, formik.touched, 'password')}
+          {toggleIcon()}
+        </div>
+
+        <Button type="submit" disabled={formik.isSubmitting} label="Login" />
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
