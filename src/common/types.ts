237232c4157447aa01306b55cea46bdea42a263c
yournightmare01 <zlatkor1001@gmail.com>
2022-06-08 16:53:15 +0200

    deleted baseURL from axios

--- src/common/types.ts
+++ src/common/types.ts
@@ -1,7 +1,7 @@
 //@Interfaces
 export interface Response<T> {
-  error_code: ResponseErrorCode | null;
-  data: T;
+  message?: ResponseErrorCode | null;
+  data?: T;
 }
 
 export interface User {
@@ -11,9 +11,8 @@ export interface User {
 
 //@Types
 export type UserLoginDataResponse = {
-  data: {
-    access_token: string;
-  } | null;
+  userId: number;
+  accessToken: string;
 };
 
 //@Enums
@@ -23,5 +22,6 @@ export enum ResponseErrorCode {
   USER_WITH_THAT_ID_DOES_NOT_EXIST = 'err003',
   NOT_AUTHORIZED = 'err004',
   INVITE_ALREADY_SENT_TO_THAT_EMAIL = 'err005',
-  INVITATION_TOKEN_NOT_VALID = 'err006'
+  INVITATION_TOKEN_NOT_VALID = 'err006',
+  USER_NOT_VERIFIED = 'err007'
 }
--- src/services/httpService.ts
+++ src/services/httpService.ts
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL,
   headers: { 'Content-Type': 'application/json' }
 });
