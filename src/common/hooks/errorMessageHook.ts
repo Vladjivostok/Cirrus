2beb8446a5d378ccc48090a784a1d6da9c341db4
Zlatko Radenkovic <zlatko.radenkovic@htecgroup.com>
2022-06-17 10:25:27 +0000

    [CJQ2TB-64] error-response messages
    *Changed responseErrorMessages according to changed contract
    
    * changed the name of the errorMessage function and changed it's directory
    
    * Added union type for errorMessage
    
    
    Approved-by: Gavrilo Aleksic
    Approved-by: Milica Djordjevic

--- src/common/hooks/errorMessageHook.ts
+++ src/common/hooks/errorMessageHook.ts
@@ -1,20 +1,10 @@
-export const useErrorMessage = (message: string) => {
-  if (message === 'err001') {
-    alert('User not found');
-  } else if (message === 'err002') {
-    alert('Not logged in');
-  } else if (message === 'err003') {
-    alert('User with that id does not exist!');
-  } else if (message === 'err004') {
-    alert('Not Authorized!');
-  } else if (message === 'err005') {
-    alert('Invite has already been sent to that email!');
-  } else if (message === 'err006') {
-    alert('Invitation token is not valid!');
-  } else if (message === 'err007') {
-    alert('User is not verified!');
+import { ResponseErrorCode } from '../types';
+
+export const errorMessageDialog = (message: ResponseErrorCode): string => {
+  if (message === 'err001' || message === 'err003' || message === 'err007') {
+    alert('Incorrect username or password');
   }
   return message;
 };
 
-export default useErrorMessage;
+export default errorMessageDialog;
