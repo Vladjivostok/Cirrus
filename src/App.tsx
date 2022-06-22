f516096867ce81430a76f0ff3a1278644a04c740
Vladimir_Mokric <vladimir.mokric@htecgroup.com>
2022-06-22 09:13:46 +0200

    Fixed problem with protected routes

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ import { updateUser } from './store/redux/auth/authSlice';
 import './App.css';
 
 const App: React.FC = () => {
+  console.log('&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&');
   const [state, setState] = useState<{ finishedChecking: boolean; isAuth: boolean }>({
     finishedChecking: false,
     isAuth: false
