116784adbb96296846d03d7c212e8f998045ce7a
Vladimir Mokric <vladimir.mokric@htecgroup.com>
2022-07-04 15:00:25 +0000

    Deploying fix for pageNotFound and registration token
    Approved-by: Zlatko Radenkovic

--- src/common/messages.ts
+++ src/common/messages.ts
@@ -1,6 +1,5 @@
 export const toastMessages = {
   successfulRegistration: 'Successfully Registered!',
   successfulEmailSent: 'Successfully sent an email! Please check your email',
-
-  successfulInvitation: 'Invitation Successfuly sent!'
+  successfulInvitation: 'Invitation successfuly sent!'
 };
